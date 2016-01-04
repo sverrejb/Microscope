c4dd8dec24e4b7c6218c28351828bcaceee2b6a4
Sverre Johann Bjørke <sverrejb@gmail.com>
2016-01-04 21:09:22 +0100

    fixed missplaced helper

--- client/templates/comments/comment_item.js
+++ client/templates/comments/comment_item.js
@@ -1,8 +1,5 @@
 Template.commentItem.helpers({
     submittedText: function() {
         return this.submitted.toString();
-    },
-    commentsCount: function() {
-        return Comments.find({postId: this._id}).count();
     }
 });
